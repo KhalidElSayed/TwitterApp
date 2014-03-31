9eaa0482126d60f58cebc9fd943aeac7e985307c
Inez Korczyński <inez@wikia-inc.com>
2014-03-31 09:16:35 -0700

    Display more details in Tweet details view

--- src/com/inez/twitterapp/TweetDetailsActivity.java
+++ src/com/inez/twitterapp/TweetDetailsActivity.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.inez.twitterapp.models.Tweet;
 import com.nostra13.universalimageloader.core.ImageLoader;
@@ -19,9 +21,26 @@ public class TweetDetailsActivity extends Activity {
 		Intent intent = getIntent();
 		Tweet tweet = (Tweet) intent.getSerializableExtra(TimelineActivity.TWEET_KEY);
 		
-		ImageView iv = (ImageView) findViewById(R.id.imageView1);
+		
+		ImageView ivProfile = (ImageView) findViewById(R.id.ivProfile);
+		ImageLoader.getInstance().displayImage(tweet.getUser().getProfileImageUrl(), ivProfile);
+		
+		TextView tvUser = (TextView) findViewById(R.id.tvUser);
+		tvUser.setText(tweet.getUser().getScreenName());
+
+		TextView tvFriends = (TextView) findViewById(R.id.tvFriends);
+		tvFriends.setText(String.valueOf(tweet.getUser().getFriendsCount()));
+
+		TextView tvFollowers = (TextView) findViewById(R.id.tvFollowers);
+		tvFollowers.setText(String.valueOf(tweet.getUser().getFollowersCount()));
+		
+		TextView tvTweet = (TextView) findViewById(R.id.tvTweet);
+		tvTweet.setText(tweet.getBody());
+
+		ImageView ivMedium = (ImageView) findViewById(R.id.ivMedium);
 		if(tweet.getMediaUrls().size() > 0) {
-			ImageLoader.getInstance().displayImage(tweet.getMediaUrls().get(0), iv);
+			ImageLoader.getInstance().displayImage(tweet.getMediaUrls().get(0), ivMedium);
+			ivMedium.setVisibility(View.VISIBLE);
 		}
 	}
 
